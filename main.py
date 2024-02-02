8eed7fc23ad260aa0133a6ac3d5fd5ed5240063c
SamoylikV <vasya.samoylik@gmail.com>
2024-02-02 17:42:10 +0300

    Author debug mode and author_procedures fixed

--- main.py
+++ main.py
@@ -53,11 +53,17 @@ class Parmaster:
         """Рассчитать количество и зарплату за авторские процедуры."""
         author_count = sum(
             count
-            for procedure_type, procedures, value in self.author_procedures.items()
-            for name, count in procedures
+            for procedure_type, procedures in self.author_procedures.items()
+            for name, count, salary_value in procedures
             if name == self.name
         )
-        author_earnings = author_count * self.RANKS[self.rank]['procedure_percentage']
+
+        author_earnings = sum(
+            salary_value
+            for procedure_type, procedures in self.author_procedures.items()
+            for name, count, salary_value in procedures
+            if name == self.name
+        )
         return author_count, author_earnings
 
     def calculate_collective_procedures(self):
@@ -106,16 +112,19 @@ def get_files(pattern):
     all_files = os.listdir()
     return [file for file in all_files if fnmatch.fnmatch(file, pattern)]
 
+
 def get_data(file, header=None):
     """Получить данные из указанного файла."""
     return pd.read_excel(file, header=header)
+
 
 def get_author_procedures(data):
     """Получить авторские процедуры из указанных данных."""
     author_procedures = {"Коллективное парение для компании": [], "Парение авторское": []}
     current_procedure = ''
 
-    for procedure_type, name, procedure, salary in zip(data.iloc[:, 0], data.iloc[:, 1], data.iloc[:, 2], data.iloc[:, 3]):
+    for procedure_type, name, procedure, salary in zip(data.iloc[:, 0], data.iloc[:, 1], data.iloc[:, 2],
+                                                       data.iloc[:, 3]):
         if str(procedure).isdigit() is False:
             continue
 
@@ -134,6 +143,8 @@ def get_author_procedures(data):
             author_procedures[current_procedure].append((name, procedure, salary_value))
 
     return author_procedures
+
+
 def get_collective_procedures(data):
     """Получить коллективные процедуры из указанных данных."""
     collective_procedures = {}
@@ -158,29 +169,32 @@ def get_collective_procedures(data):
 
     return {key: value for key, value in collective_procedures.items() if value}
 
-def get_parmasters_info(data):
-    """Получить информацию о Parmasters из указанных данных."""
-    return data[data.iloc[:, 3].notna() & data.iloc[:, 4].notna()]
 
-def get_parmasters(parmasters_info, total_procedures, author_procedures, collective_procedures):
+def get_parmasters_info(data, data2):
+    """Получить информацию о Parmasters из указанных данных."""
+    return data[data.iloc[:, 3].notna() & data.iloc[:, 4].notna()], data2.iloc[:, 0], data2.iloc[:, 1], data2.iloc[:, 2], data2.iloc[:, 3]
+
+
+def get_parmasters(parmasters_info, total_procedures, author_procedures, collective_procedures, debug_mode=False):
     """Получить Parmasters из указанной информации."""
     parmasters = []
     valid_ranks = ['стажер', 'младший мастер', 'мастер', 'старший мастер']
-    parmasters_names = parmasters_info.iloc[:, 3].dropna().unique()
+    parmasters_names = parmasters_info[0].iloc[:, 3].dropna().unique()
 
     for name in parmasters_names:
         if name != 'Продано с блюдом':
-            # questions = [
-            #     inquirer.List('rank',
-            #                   message=f"Выберите ставку сотрудника {name}",
-            #                   choices=valid_ranks,
-            #                   ),
-            # ]
-            # answers = inquirer.prompt(questions)
-            # rank = answers['rank']
-            rank = 'мастер'
-            parmaster_info = parmasters_info[parmasters_info.iloc[:, 1] == name]
-
+            if debug_mode:
+                rank = 'мастер'
+            else:
+                questions = [
+                    inquirer.List('rank',
+                                  message=f"Выберите ставку сотрудника {name}",
+                                  choices=valid_ranks,
+                                  ),
+                ]
+                answers = inquirer.prompt(questions)
+                rank = answers['rank']
+            parmaster_info = parmasters_info[0][parmasters_info[0].iloc[:, 1] == name]
             for index, row in parmaster_info.iterrows():
                 procedure_type = row[0]
                 try:
@@ -188,23 +202,30 @@ def get_parmasters(parmasters_info, total_procedures, author_procedures, collect
                 except ValueError:
                     count = 0
                 collective_procedures.setdefault(procedure_type, []).append((name, count))
-            parmaster = Parmaster(name, rank, total_procedures, author_procedures, collective_procedures)
 
+            parmaster = Parmaster(name, rank, total_procedures, author_procedures, collective_procedures)
             parmasters.append(parmaster)
 
     return parmasters
 
+
 def get_results(parmasters):
     """Получить результаты от указанных Parmasters."""
-    return [{'Имя': parmaster.name, 'Ставка': parmaster.rank, 'Зарплата': parmaster.calculate_salary()} for parmaster in parmasters]
+    return [{'Имя': parmaster.name, 'Ставка': parmaster.rank, 'Зарплата': parmaster.calculate_salary()} for parmaster in
+            parmasters]
+
 
 def save_results(results, file_name):
     """Сохранить результаты в указанный файл."""
     results_df = pd.DataFrame(results)
     results_df.to_excel(file_name, index=False)
 
+
 def main():
     """Основная функция."""
+
+    debug_mode = False # Включить/выключить режим отладки
+
     data1_files = get_files('!1*.xlsx')
     data2_files = get_files('!3*.xlsx')
     data3_files = get_files('!4*.xlsx')
@@ -219,25 +240,23 @@ def main():
         data3 = data3_files[0]
 
     data1_df = get_data(data1)
-    data2_df = get_data(data2)
+    data2_df = get_data(data2, header=3)
     data3_df = get_data(data3, header=3)
 
     collective_procedures = get_collective_procedures(data3_df)
     author_procedures = get_author_procedures(data2_df)
 
-
     total_procedures = data1_df.iloc[-1].iloc[2]
 
-    parmasters_info = get_parmasters_info(data3_df)
-
-    parmasters = get_parmasters(parmasters_info, total_procedures, author_procedures, collective_procedures)
+    parmasters_info = get_parmasters_info(data3_df, data2_df)
+    parmasters = get_parmasters(parmasters_info, total_procedures, author_procedures, collective_procedures, debug_mode)
 
     results = get_results(parmasters)
-
     date_time = re.search(r'\d{2}_\d{2}_\d{4}_\d{2}_\d{2}_\d{2}', data3).group()
     result_file_name = f'отчет_{date_time}.xlsx'
 
     save_results(results, result_file_name)
+
 
 if __name__ == "__main__":
     main()
